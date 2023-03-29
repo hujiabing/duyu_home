d4453f977504f0c550e94ac85739104a250c6986
hujiabing <hujiabing@autohome.com.cn>
2023-03-29 14:53:28 +0800

    初始化

---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+</script>
+
+<template>
+  <div class="pager-container">
+
+  </div>
+</template>
+
+<style lang="scss">
+.pager-container{
+  width: 100px;
+  height: 100px;
+  background-color: antiquewhite;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,23 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 两种方式都可以
+        additionalData: '@import "@/assets/scss/global.scss";'
+        // additionalData: '@use "@/assets/scss/global.scss" as *;'
+      }
+    }
+  },
+})
